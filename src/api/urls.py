b61463eea4b3e1e21bc525ff8c4270bfbe6faf78
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-05-20 09:54:46 -0600

    Solution

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 from api import views
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r"users", views.UserViewSet)
